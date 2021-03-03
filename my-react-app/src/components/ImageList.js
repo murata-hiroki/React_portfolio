f1575f2c87d818a9f7756f59531d8b028b2134d9
murata-hiroki <hirokinaoki2000@gmail.com>
2021-03-03 23:20:53 +0900

    フォルダー名変更

---
+++ my-react-app/src/components/ImageList.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Masonry from 'react-masonry-component';
+
+const ImageList = (props) => {
+    const images = props.images.map((image)=>{
+      return(
+        <a href ={image.pageURL}
+        key={image.id}
+        target='_blank' 
+        rel='noopener noreferrer' 
+        className='ui medium image'
+        >
+          <img src ={image.webformatURL} alt={image.tags}/>
+        </a>
+      );
+    });
+  return (
+    <Masonry className = 'image-list'>
+    {images}
+  </Masonry>
+    );
+};
+
+export default ImageList;
